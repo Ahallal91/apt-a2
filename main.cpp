2fdf4e01d90f09061badf6b0b680cbbc1c6adcd8
s3783156 <s3783156@student.rmit.edu.au>
2020-09-01 21:20:23 +1000

    Cleaned up file

--- main.cpp
+++ main.cpp
@@ -4,46 +4,43 @@ void runMenu();
 void menuText();
 
 int main(int argc, char** argv) {
-    runMenu();
-    return EXIT_SUCCESS;
+	runMenu();
+	return EXIT_SUCCESS;
 }
 
 void runMenu() {
-    bool exitMenu = false;
-    const std::string choice1 = "1";
-    const std::string choice2 = "2";
-    const std::string choice3 = "3";
-    const std::string choice4 = "4";
-    const std::string endOfFile = "^D";
+	bool exitMenu = false;
+	const std::string choice1 = "1";
+	const std::string choice2 = "2";
+	const std::string choice3 = "3";
+	const std::string choice4 = "4";
 
-    while(!exitMenu) {
-        menuText();
-        std::string choice;
-        std::cin >> choice;
-        if(choice == choice1) {
+	while (!exitMenu) {
+		menuText();
+		std::string choice;
+		std::cin >> choice;
+		if (choice == choice1) {
 
-        } else if (choice == choice2) {
+		} else if (choice == choice2) {
 
-        } else if (choice == choice3) {
+		} else if (choice == choice3) {
 
-        } else if (std::cin.eof() || 
-                    choice == choice4 || 
-                    choice == endOfFile) {
-            exitMenu = true;
-        } else {
-            std::cout << "Please select a valid option" << std::endl;
-        }    
-    }
+		} else if (std::cin.eof() || choice == choice4) {
+			exitMenu = true;
+		} else {
+			std::cout << "Please select a valid option" << std::endl;
+		}
+	}
 }
 
 void menuText() {
-    std::cout << "Welcome to Azul!" << std::endl;
-    std::cout << "-------------------" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Menu" << std::endl;
-    std::cout << "----" << std::endl;
-    std::cout << "1. New Game" << std::endl;
-    std::cout << "2. Load Game" << std::endl;
-    std::cout << "3. Credits (Show student information)" << std::endl;
-    std::cout << "4. Quit" << std::endl;
+	std::cout << "Welcome to Azul!" << std::endl;
+	std::cout << "-------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Menu" << std::endl;
+	std::cout << "----" << std::endl;
+	std::cout << "1. New Game" << std::endl;
+	std::cout << "2. Load Game" << std::endl;
+	std::cout << "3. Credits (Show student information)" << std::endl;
+	std::cout << "4. Quit" << std::endl;
 }
