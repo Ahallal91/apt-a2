4b0622c58aa2d551c107dbac4856e9b1aa4acf11
s3780973 <s3780973@student.rmit.edu.au>
2020-09-06 23:43:34 +1000

    Implementing the gameplay input method to be used validate user inputs during the game

--- Input.cpp
+++ Input.cpp
@@ -19,7 +19,128 @@ Player* Input::enterPlayerName(int playerNum) {
 	return new Player(name);
 }
 
-// TODO return boolean if user entered EOF character
-void Input::getTurn() {
+std::vector<std::string> Input::getGameplayInput() {
 
+	// A string containing the full line a user inputted
+	std::string input;
+
+	// Checks whether eof was entered to quit the game
+	bool eof = false;
+	
+	// Gets the input of the user one char at a time while checking for EOF
+	bool entered = false;
+	while(!entered) {
+		if(std::cin.eof()) {
+			entered = true;
+			eof = true;
+		}
+
+		char c = std::cin.get();
+		if(c != '\n') {
+			input = input + c;
+		} else {
+			entered = true;
+		}
+	}
+
+	// The arguments entered in the users command
+	std::vector<std::string> arguments = explode(input);
+	std::string command = arguments[0];
+	
+	// Checks whether an input is valid
+	bool valid = false;
+	
+	// Turn command
+	if(command == "turn") {
+		// Validate turn command | FORMAT: turn [0-5] [RYDBU] [1-5]
+		if(arguments.size() == TURN_ARGUMENTS) {
+			valid = validateTurnCommand(arguments);
+		}
+	
+	// Save command
+	} else if(command == "save") {
+		// Validate save command (FORMAT: save [filename])
+		if(arguments.size() == SAVE_ARGUMENTS) {
+			valid = validateSaveCommand(arguments);
+		}
+	}
+
+	// If the input is not valid in any way, return an empty vector
+	if(!valid) {
+		arguments.clear();
+	}
+
+	// If EOF was entered, set the return argument to "quit"
+	if(eof) {
+		arguments = explode("quit");
+	}
+
+	return arguments;
+}
+
+bool Input::validateTurnCommand(std::vector<std::string> arguments) {
+	bool factoryValid = false;
+	bool tileValid = false;
+	bool rowValid = false;
+
+	std::string factoryStr = arguments[1];
+	std::string tileStr = arguments[2];
+	std::string rowStr = arguments[3];
+	
+	// Validate the factory (2nd parameter) is a number 0 - 5
+	try {
+		int factory = std::stoi(factoryStr);
+		factoryValid = (factory >= 0 && factory <= WALL_DIM);
+	} catch(std::exception e) {}
+	
+	// Validate that the tile (3rd parameter) is a tile char
+	if(tileStr.length() == 1) {
+		char tile = tileStr[0];
+		tileValid = (tile == RED || tile == YELLOW || tile == DARK_BLUE || tile == LIGHT_BLUE || tile == BLACK);
+	}
+
+	// Validate the row / pattern line (4th parameter) is a number 1 - 5
+	try {
+		int row = std::stoi(rowStr);
+		rowValid = (row > 0 && row <= WALL_DIM);
+	} catch(std::exception e) {}
+
+	return (factoryValid && tileValid && rowValid);
+}
+
+bool Input::validateSaveCommand(std::vector<std::string> arguments) {
+	bool filenameValid = true;
+
+	std::string filename = arguments[1];
+
+	// Characters not allowed in filenames
+	char invalidCharacters[] = {'<', '>', ':', '"', '/', '\\', '|', '?', '*'};
+
+	for(char c : invalidCharacters) {
+		if(filename.find(c) != std::string::npos) {
+			filenameValid = false;
+		}
+	}
+
+	return filenameValid;
+}
+
+std::vector<std::string> Input::explode(std::string str) {
+	
+	// Vector of the command keywords seperated by white space
+	std::vector<std::string> arguments;
+
+	std::string word = "";
+
+	for(char c : str) {
+		if(c == ' ') {
+			arguments.push_back(word);
+			word = "";
+		} else {
+			word = word + c;
+		}
+	}
+	arguments.push_back(word);
+
+	return arguments;
 }
--- Input.h
+++ Input.h
@@ -1,6 +1,9 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include <vector>
+#include <string>
+
 class Player;
 // used for getting and validating input.
 class Input {
@@ -9,10 +12,30 @@ public:
 	~Input();
 
 	Player* enterPlayerName(int playerNum);
-	void getTurn();
-
+	
+	// Requests the user to enter a command during the game and validates the commands arguments
+	//
+	// Returns a vector of the arguments entered by the user ONLY IF the command is valid.
+	// If user enters EOF, will return vector containing quit string
+	// If the command was NOT valid in any way, returns an empty vector
+	//
+	// Example inputs:
+	// "hello" - not valid (hello not a command)
+	// "turn 7 P 8 9" - not valid (numbers too big for board, P not a tile and too many arguments)
+	// "turn 3 Y 1" - valid
+	// "save azulgame" - valid
+	// "save azulgame?/*" - not valid (not allowed question mark, forward slash or asterix in filenames)
+	std::vector<std::string> getGameplayInput();
 
 private:
+	// Validates the turn command parameters
+	bool validateTurnCommand(std::vector<std::string> arguments);
+
+	// Validates the save command parameters
+	bool validateSaveCommand(std::vector<std::string> arguments);
+
+	// A helper function to split a string into a vector of strings deliminated by white space
+	std::vector<std::string> explode(std::string input);
 
 };
 
