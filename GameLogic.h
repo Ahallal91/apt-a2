f413b70dabbd099cd4e1f5f356c09d7e8b9f27bd
Alicia Hallal <S3811836@student.rmit.edu.au>
2020-09-06 19:56:56 +1000

    completed logic for taking tiles

--- GameLogic.h
+++ GameLogic.h
@@ -3,16 +3,19 @@
 
 class Factories;
 class TileBag;
+class Player;
 
 class GameLogic {
 public:
 	GameLogic();
 	~GameLogic();
 	void initFactoryTiles(Factories* factories, TileBag* tileBag);
-	char* takeTilesFromFactory(Factories* factories, int factoryNumber, char tile);
+	bool takeTiles(Factories* factories, Player* player, int factoryNumber,
+					 char tile, int destPatternLine);
 
 private:
-
+	char* takeTilesFromFactory(Factories* factories, int factoryNumber, char tile);
+	bool playerTileCheck(Player* player, char tile, int destPatternLine);
 };
 
 #endif // GAME_LOGIC_H
